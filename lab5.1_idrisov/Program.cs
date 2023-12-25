0097b47c5467cf7bdbcf3b5e1ce5e447fbca0763
Platinum908 <estnuntius@gmail.com>
2023-12-25 03:03:15 +0200

    Добавьте файлы проекта.

---
+++ lab5.1_idrisov/Program.cs
@@ -0,0 +1,34 @@
+﻿//Лаб 5.1 Высокий 20
+
+Console.WriteLine("Введите текст до 100 слов: ");
+char[] mas = Console.ReadLine().ToLower().ToCharArray();
+for (int i = 0; i < 33; i++)
+{
+    int x = 0;
+    int y = 0;
+    for (int j = 0; j < mas.Length && Char.IsLetter(mas[j]) != true; j++) x++;
+    for (int j = 0; j < mas.Length; j++)
+    {
+        if (Char.IsLetter(mas[i]) == true && mas[j] == mas[x])
+        {
+            y++;
+            if (x != j) mas[j] = ' ';
+        }
+    }
+
+    if (y > 0)
+    {
+        switch (y % 10)
+        {
+            case 2:
+            case 3:
+            case 4:
+                Console.WriteLine($"буква {mas[x]} = {y} раза");
+                break;
+            default:
+                Console.WriteLine($"буква {mas[x]} = {y} раз");
+                break;
+        }
+        mas[x] = ' ';
+    }
+}
